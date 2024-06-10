88e7d54d6a85358cf7778291e156e4ccfd1cdf24
TripleAn <83976324+triplean@users.noreply.github.com>
2024-06-10 17:32:59 -0600

    1

--- sh/script.js
+++ sh/script.js
@@ -1,10 +1,3 @@
-var counterVal = 0;
-
 function descargar_button() {
-  updateDisplay(++counterVal);
   window.location.href="https://static-4e34f.web.app/downloads/Security_Helpy.zip";
-}
-
-function updateDisplay(val) {
-  document.getElementById('counter-label').innerHTML = val;
 }
